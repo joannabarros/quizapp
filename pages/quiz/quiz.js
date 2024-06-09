ab830ea57dd84d95e1985d82e4d58ffe36d1deb5
Joanna Reis 22A <joanna.barros16@gmail.com>
2024-06-09 22:43:56 +0000

    adicionada a função de jogar novamente e FINALIZAÇÃO DO PROJETO

--- pages/quiz/quiz.js
+++ pages/quiz/quiz.js
@@ -136,7 +136,7 @@ function validarResposta () {
 function finalizar(){
     localStorage.setItem("pontos", pontos)
 
-    window.location.href = "../resultado/resultado.html"
+    window.location.href = "../../resultado.html"
 }
 
 function proximaPergunta() {
